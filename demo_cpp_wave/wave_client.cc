9ff10a8766f4a9196452139a4393c9fc965c6e01
Lea Lincker <lea.lincker@sirehna.com>
2019-04-18 14:14:48 +0200

    Repositionned curly braces to match x-dyn convention

--- demo_cpp_wave/wave_client.cc
+++ demo_cpp_wave/wave_client.cc
@@ -18,78 +18,88 @@ using wave::Elevation;
 using wave::WaveService;
 
 void display_elevations(const std::vector<double>& z, const std::vector<double>& x, const std::vector<double>& y, const double t);
-void display_elevations(const std::vector<double>& z, const std::vector<double>& x, const std::vector<double>& y, const double t) {
-  if (z.size() > 0){
-    for (size_t index = 0; index < z.size(); ++index) {
-      std::cout << "WaveService (x: " << x[index] << ", y: " << y[index] << ", t: " << t <<  ") received: " << z[index] << std::endl;
+void display_elevations(const std::vector<double>& z, const std::vector<double>& x, const std::vector<double>& y, const double t)
+{
+    if (z.size() > 0)
+    {
+        for (size_t index = 0; index < z.size(); ++index)
+        {
+            std::cout << "WaveService (x: " << x[index] << ", y: " << y[index] << ", t: " << t <<  ") received: " << z[index] << std::endl;
+        }
     }
-  } else {
-    std::cout << "WaveService received no data." << std::endl;
-  }
+    else
+    {
+        std::cout << "WaveService received no data." << std::endl;
+    }
 }
 
-class WaveServiceClient {
- public:
-  WaveServiceClient(std::shared_ptr<Channel> channel)
-      : stub_(WaveService::NewStub(channel)) {}
+class WaveServiceClient
+{
+    public:
+    WaveServiceClient(std::shared_ptr<Channel> channel)
+        : stub_(WaveService::NewStub(channel)) {}
 
-  void get_elevation(const std::vector<double>& x, const std::vector<double>& y, const double t) {
-    Point request;
-    const size_t max_size = std::min(x.size(), y.size());
-    for (size_t index = 0; index < max_size; ++index) {
-      request.add_x(x[index]);
-      request.add_y(y[index]);
+    void get_elevation(const std::vector<double>& x, const std::vector<double>& y, const double t)
+    {
+        Point request;
+        const size_t max_size = std::min(x.size(), y.size());
+        for (size_t index = 0; index < max_size; ++index)
+        {
+            request.add_x(x[index]);
+            request.add_y(y[index]);
+        }
+        request.set_t(t);
+
+        Elevation reply;
+        ClientContext context;
+
+        Status status = stub_->GetElevation(&context, request, &reply);
+        if (status.ok())
+        {
+            std::vector<double> z(reply.z().begin(), reply.z().end());
+            display_elevations(z, x, y, t);
+        }
+        else
+        {
+            std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+            std::cout << "WaveService failed." << std::endl;
+        }
     }
-    request.set_t(t);
 
-    Elevation reply;
+    void get_elevations(const std::vector<double>& x, const std::vector<double>& y,
+                        const double dt, const double t_start, const double t_end)
+    {
+        Point request;
+        const size_t max_size = std::min(x.size(), y.size());
+        for (size_t index = 0; index < max_size; ++index)
+        {
+            request.add_x(x[index]);
+            request.add_y(y[index]);
+        }
+        request.set_dt(dt);
+        request.set_t_start(t_start);
+        request.set_t_end(t_end);
 
-    ClientContext context;
+        Elevation elevation;
+        ClientContext context;
 
-    Status status = stub_->GetElevation(&context, request, &reply);
+        std::unique_ptr<ClientReader<Elevation> > reader(stub_->GetElevations(&context, request));
+        while (reader->Read(&elevation))
+        {
+            std::vector<double> z(elevation.z().begin(), elevation.z().end());
+            display_elevations(z, x, y, elevation.t());
+        }
+        Status status = reader->Finish();
 
-    if (status.ok()) {
-      std::vector<double> z(reply.z().begin(), reply.z().end());
-      display_elevations(z, x, y, t);
-    } else {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      std::cout << "WaveService failed." << std::endl;
+        if (!status.ok())
+        {
+            std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+            std::cout << "WaveService failed." << std::endl;
+        }
     }
-  }
 
-  void get_elevations(const std::vector<double>& x, const std::vector<double>& y,
-                           const double dt, const double t_start, const double t_end) {
-    Point request;
-    const size_t max_size = std::min(x.size(), y.size());
-    for (size_t index = 0; index < max_size; ++index) {
-      request.add_x(x[index]);
-      request.add_y(y[index]);
-    }
-    request.set_dt(dt);
-    request.set_t_start(t_start);
-    request.set_t_end(t_end);
-
-    Elevation elevation;
-
-    ClientContext context;
-
-    std::unique_ptr<ClientReader<Elevation> > reader(stub_->GetElevations(&context, request));
-    while (reader->Read(&elevation)) {
-      std::vector<double> z(elevation.z().begin(), elevation.z().end());
-      display_elevations(z, x, y, elevation.t());
-    }
-    Status status = reader->Finish();
-
-    if (!status.ok()) {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      std::cout << "WaveService failed." << std::endl;
-    }
-  }
-
-  private:
-    std::unique_ptr<WaveService::Stub> stub_;
+    private:
+        std::unique_ptr<WaveService::Stub> stub_;
 };
 
 
@@ -122,8 +132,13 @@ int main(int argc, char const * const argv[])
     }
     std::string port("50051");
     std::string ip("localhost");
-    if (input_port) { std::cout << "input_port: " << args::get(input_port) << std::endl; port = std::to_string(args::get(input_port));}
-    if (input_ip) { std::cout << "input_ip: " << args::get(input_ip) << std::endl; ip = args::get(input_ip); }
+    if (input_port)
+    {
+      std::cout << "input_port: " << args::get(input_port) << std::endl; port = std::to_string(args::get(input_port));
+    }
+    if (input_ip) {
+      std::cout << "input_ip: " << args::get(input_ip) << std::endl; ip = args::get(input_ip);
+    }
 
     std::cout << "Client" << std::endl;
     WaveServiceClient wave_service(grpc::CreateChannel(
--- demo_cpp_wave/wave_server.cc
+++ demo_cpp_wave/wave_server.cc
@@ -23,80 +23,86 @@ using wave::FlatDiscreteDirectionalWaveSpectrum;
 
 
 class WaveServiceImpl final : public WaveService::Service {
-  public:
-    explicit WaveServiceImpl(const FlatDiscreteDirectionalWaveSpectrum& wave_spectrum):
-      wave_spectrum_(wave_spectrum)
-    {
-    }
+    public:
+        explicit WaveServiceImpl(const FlatDiscreteDirectionalWaveSpectrum& wave_spectrum):
+            wave_spectrum_(wave_spectrum) {}
 
-    Status GetElevation(ServerContext* context, const Point* request,
-                    Elevation* reply) override {
-      reply->clear_z();
+        Status GetElevation(ServerContext* context, const Point* request,
+                            Elevation* reply) override
+        {
+            reply->clear_z();
 
-      const size_t max_size = std::min(request->x_size(), request->y_size());
-      for (size_t index = 0; index < max_size; ++index) {
-        reply->add_z(- wave_spectrum_.a() * sin(
-                          wave_spectrum_.k() * (request->x(index) * cos(wave_spectrum_.psi()) + request->y(index) * sin(wave_spectrum_.psi()))
-                          - wave_spectrum_.omega() * request->t()
-                          + wave_spectrum_.phase()
-                      )
-                    );
-      }
+            const size_t max_size = std::min(request->x_size(), request->y_size());
+            for (size_t index = 0; index < max_size; ++index)
+            {
+                reply->add_z(- wave_spectrum_.a() * sin(
+                                    wave_spectrum_.k() * (request->x(index) * cos(wave_spectrum_.psi()) + request->y(index) * sin(wave_spectrum_.psi()))
+                                    - wave_spectrum_.omega() * request->t()
+                                    + wave_spectrum_.phase()
+                                )
+                            );
+            }
 
-      return Status::OK;
-    }
-
-    Status GetElevations(ServerContext* context, const Point* request,
-                        ServerWriter<Elevation>* writer) override {
-      Elevation elevation;
-      const size_t max_size = std::min(request->x_size(), request->y_size());
-      for (double t = request->t_start(); t < request->t_end(); t = t + request->dt()) {
-        elevation.clear_z();
-        elevation.set_t(t);
-        for (size_t index = 0; index < max_size; ++index) {
-          elevation.add_z(- wave_spectrum_.a() * sin(
-                                wave_spectrum_.k() * (request->x(index) * cos(wave_spectrum_.psi()) + request->y(index) * sin(wave_spectrum_.psi()))
-                                - wave_spectrum_.omega() * t
-                                + wave_spectrum_.phase()
-                            )
-                          );
+            return Status::OK;
         }
-        writer->Write(elevation);
-      }
-      return Status::OK;
-    }
+
+        Status GetElevations(ServerContext* context, const Point* request,
+                            ServerWriter<Elevation>* writer) override
+        {
+            Elevation elevation;
+            const size_t max_size = std::min(request->x_size(), request->y_size());
+            for (double t = request->t_start(); t < request->t_end(); t = t + request->dt())
+            {
+                elevation.clear_z();
+                elevation.set_t(t);
+                for (size_t index = 0; index < max_size; ++index)
+                {
+                    elevation.add_z(- wave_spectrum_.a() * sin(
+                                            wave_spectrum_.k() * (request->x(index) * cos(wave_spectrum_.psi()) + request->y(index) * sin(wave_spectrum_.psi()))
+                                            - wave_spectrum_.omega() * t
+                                            + wave_spectrum_.phase()
+                                        )
+                                    );
+                }
+                writer->Write(elevation);
+            }
+            return Status::OK;
+        }
 
     private:
         FlatDiscreteDirectionalWaveSpectrum wave_spectrum_;
 };
 
-void compute_wave_sectrum(FlatDiscreteDirectionalWaveSpectrum& wave_spectrum);
-void compute_wave_sectrum(FlatDiscreteDirectionalWaveSpectrum& wave_spectrum) {
-  wave_spectrum.set_a(2.0);
-  wave_spectrum.set_omega(2 * PI / 12);
-  wave_spectrum.set_psi(PI / 4);
-  wave_spectrum.set_k(wave_spectrum.omega() * wave_spectrum.omega() / G);
-  wave_spectrum.set_phase(0.0);
+void compute_wave_spectrum(FlatDiscreteDirectionalWaveSpectrum& wave_spectrum);
+void compute_wave_spectrum(FlatDiscreteDirectionalWaveSpectrum& wave_spectrum)
+{
+    wave_spectrum.set_a(2.0);
+    wave_spectrum.set_omega(2 * PI / 12);
+    wave_spectrum.set_psi(PI / 4);
+    wave_spectrum.set_k(wave_spectrum.omega() * wave_spectrum.omega() / G);
+    wave_spectrum.set_phase(0.0);
 }
 
 void run_server(const FlatDiscreteDirectionalWaveSpectrum& wave_spectrum);
-void run_server(const FlatDiscreteDirectionalWaveSpectrum& wave_spectrum) {
-  std::string server_address("0.0.0.0:50051");
-  WaveServiceImpl service(wave_spectrum);
+void run_server(const FlatDiscreteDirectionalWaveSpectrum& wave_spectrum)
+{
+    std::string server_address("0.0.0.0:50051");
+    WaveServiceImpl service(wave_spectrum);
 
-  ServerBuilder builder;
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  builder.RegisterService(&service);
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << "Server listening on " << server_address << std::endl;
-  server->Wait();
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+    server->Wait();
 }
 
-int main(int argc, char** argv) {
-  FlatDiscreteDirectionalWaveSpectrum wave_spectrum;
-  compute_wave_sectrum(wave_spectrum);
+int main(int argc, char** argv)
+{
+    FlatDiscreteDirectionalWaveSpectrum wave_spectrum;
+    compute_wave_spectrum(wave_spectrum);
 
-  run_server(wave_spectrum);
+    run_server(wave_spectrum);
 
-  return 0;
+    return 0;
 }
