71774c06593d440ec6701667a725179892637e6b
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2019-04-23 08:47:14 +0200

    [bazel] Removed ifdef instructions related to bazel in greeter_sync_server.cc

--- demo_cpp_hello_world/greeter_sync_server.cc
+++ demo_cpp_hello_world/greeter_sync_server.cc
@@ -22,11 +22,7 @@
 
 #include <grpcpp/grpcpp.h>
 
-#ifdef BAZEL_BUILD
-#include "examples/protos/helloworld.grpc.pb.h"
-#else
 #include "helloworld.grpc.pb.h"
-#endif
 
 using grpc::Server;
 using grpc::ServerBuilder;
