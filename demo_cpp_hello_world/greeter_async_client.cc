98beb7471e73d289cddcd0715c9e1cb654a731d8
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2019-04-23 08:47:12 +0200

    [bazel] Removed ifdef instructions related to bazel in greeter_async_client.cc

--- demo_cpp_hello_world/greeter_async_client.cc
+++ demo_cpp_hello_world/greeter_async_client.cc
@@ -25,11 +25,7 @@
 
 #include <args.hxx>
 
-#ifdef BAZEL_BUILD
-#include "examples/protos/helloworld.grpc.pb.h"
-#else
 #include "helloworld.grpc.pb.h"
-#endif
 
 using grpc::Channel;
 using grpc::ClientAsyncResponseReader;
