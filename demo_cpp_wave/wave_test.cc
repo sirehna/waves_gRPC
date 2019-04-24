a0da0ae3cf01c94267fb42c54e568d07b02688a0
Lea Lincker <lea.lincker@sirehna.com>
2019-04-24 17:43:25 +0200

    [gtest] Improved first test: add more points and repeat resquest 10 times

--- demo_cpp_wave/wave_test.cc
+++ demo_cpp_wave/wave_test.cc
@@ -4,25 +4,34 @@
 
 class ServerDemo : public ::testing::Test
 {
+    protected:
+        void SetUp() override {
+            port = "50051";
+            ip = "server";
+        }
+
+        std::string port;
+        std::string ip;
 };
 
 TEST_F(ServerDemo, get_elevation_demo)
 {
-    std::string port("50051");
-    std::string ip("server");
     ElevationServiceClient elevation_service(grpc::CreateChannel(
         ip + ":" + port, grpc::InsecureChannelCredentials()));
 
-    const std::vector<double> x{1.3, 2, 0};
-    const std::vector<double> y{2.7, 0.5, 0};
+    const std::vector<double> x{1.3, 2, 0, 2.7, 0.5, 0.1, 1.3, 2, 0, 2.7, 0.5, 0.1, 1.3, 2, 0, 2.7, 0.5, 0.1, 1.3, 2};
+    const std::vector<double> y{2.7, 0.5, 0, 1.3, 2, 0.2, 1.3, 2, 1, 2.7, 0.5, 0.1, 2.7, 0.5, 0, 1.3, 2, 0.2, 1.3, 2};
     const double t(0.1);
 
-    auto start = std::chrono::system_clock::now();
+    std::vector<std::chrono::duration<double>> times{};
 
-    ElevationResponse reply = elevation_service.get_elevation(x, y, t);
-
-    std::chrono::duration<double> diff = std::chrono::system_clock::now() - start;
-    std::cout << "elevation_service.get_elevation took " << diff.count() << " s." << std::endl;
-
-    EXPECT_DOUBLE_EQ(reply.elevation_points(0).z(), -0.053363484126936325);
+    for (size_t index = 0; index < 10; ++index)
+    {
+        auto start = std::chrono::system_clock::now();
+        ElevationResponse reply = elevation_service.get_elevation(x, y, t);
+        const std::chrono::duration<double> diff = std::chrono::system_clock::now() - start;
+        times.push_back(diff);
+        std::cout << "Request duration: " << diff.count() << " s." << std::endl;
+        EXPECT_DOUBLE_EQ(reply.elevation_points(0).z(), 1.2605762463595371);
+    }
 }
