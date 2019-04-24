3300ba4b76ed5cc823b53cbb66567d823a5090f0
Lea Lincker <lea.lincker@sirehna.com>
2019-04-24 16:31:58 +0200

    [gtest] Created gtest docker-compose skeleton

---
+++ demo_cpp_wave/wave_test.cc
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+#include "wave_client.hh"
+
+class ServerDemo : public ::testing::Test
+{
+};
+
+TEST_F(ServerDemo, say_hello_demo)
+{
+    std::string port("50051");
+    std::string ip("server");
+    GreeterClient greeter(grpc::CreateChannel(
+        ip + ":" + port, grpc::InsecureChannelCredentials()));
+    std::string user("world");
+    std::string reply = greeter.SayHello(user);
+    ASSERT_EQ(reply, "Hello world");
+}
