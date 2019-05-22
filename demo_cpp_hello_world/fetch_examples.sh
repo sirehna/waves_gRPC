86ce55719eb16a03db630bf6f18c2a74f6693311
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2019-05-22 16:06:00 +0200

    Removed useless demo_cpp_hello_world

--- demo_cpp_hello_world/fetch_examples.sh
+++
@@ -1,7 +0,0 @@
-mkdir grpc_src
-cd grpc_src
-git init
-git remote add -f origin https://github.com/grpc/grpc
-git config core.sparseCheckout true
-echo "examples/cpp/helloworld" >> .git/info/sparse-checkout
-git pull origin master
